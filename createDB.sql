46049feb5c34ffc21f3745925a550d23011b85fc
ishan-agrawal223 <47202797+ishan-agrawal223@users.noreply.github.com>
2019-03-22 10:59:27 +0530

    Update createDB.sql

--- createDB.sql
+++ createDB.sql
@@ -1,29 +1,19 @@
+drop table tskill;
+drop table sskill;
+drop table cskill;
+
+
+
 drop table adminDB;
 drop table allocationDB;
 drop table smeDB;
 drop table courseDB;
 drop table skillDB;
 drop table trainerDB;
-drop table tskill;
-drop table sskill;
 
 
 
-create table tskill(
-tId int not null,
-skillId  int not null,
-primary key(skillId,tId),
-foreign key(tId)
-references trainerDB(tId));
 
-
-
-create table sskill(
-sId int not null,
-skillId  int not null,
-primary key(skillId,sId),
-foreign key(sId)
-references smeDB(sId));
 
 
 
@@ -43,6 +33,7 @@ tLastName varchar(20) not null,
 tAge int not null,
 tGender varchar(10) not null,
 tContactNo varchar(13) not null,
+tMail varchar(15) not null,
 Username varchar(20) not null unique,
 Password varchar(20) not null,
 tDateBegin date	not null,
@@ -60,10 +51,12 @@ sLastName varchar(20) not null,
 sAge int not null,
 sGender varchar(10) not null,
 sContactNo varchar(13) not null,
+tMail varchar(15) not null,
 Username varchar(20) not null unique,
 Password varchar(20) not null,
 sDateBegin date	not null,
 sDateEnd date not null,
+sstatus int not null,
 primary key(sId));
 
 
@@ -102,7 +95,46 @@ references skillDB(skillId));
 
 
 
+
+create table tskill(
+tId int not null,
+skillId  int not null,
+primary key(skillId,tId),
+foreign key(tId)
+references trainerDB(tId),
+foreign key(skillId)
+references skillDB(skillId));
+
+
+
+
+
+create table sskill(
+sId int not null,
+skillId  int not null,
+primary key(skillId,sId),
+foreign key(sId)
+references smeDB(sId),
+foreign key(skillId)
+references skillDB(skillId));
+
+
+
+
+
+create table cskill(
+cId int not null,
+skillId  int not null,
+primary key(skillId,cId),
+foreign key(skillId)
+references skillDB(skillId),
+foreign key(cId)
+references courseDB(cId));
+
+
 	
+
+
 insert into skillDB(skillId,skillName) values (1,'C');
 insert into skillDB(skillId,skillName) values (2,'C++');
 insert into skillDB(skillId,skillName) values (3,'Java');
@@ -111,3 +143,7 @@ insert into skillDB(skillId,skillName) values (3,'Java');
 
 
 insert into adminDB(Username,Password) values ('Admin','Password');
+
+
+
+commit;
