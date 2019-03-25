74d10d20f5bb022d8df1677b5806152f7dc84829
Chhavi Sharma <chhavi0802@gmail.com>
2019-03-25 10:40:07 +0530

    Update createDB.sql

--- createDB.sql
+++ createDB.sql
@@ -1,3 +1,4 @@
+drop sequence n;
 drop table tskill;
 drop table sskill;
 drop table cskill;
@@ -147,11 +148,25 @@ insert into skillDB(skillId,skillName) values (1,'C');
 insert into skillDB(skillId,skillName) values (2,'C++');
 insert into skillDB(skillId,skillName) values (3,'Java');
 
-
+insert into courseDB(cId,cName,cDateBegin,cDateEnd) values(1,'course1',to_date('01/08/1999','MM/dd/yyyy'),to_date('18/08/2002','MM/dd/yyyy'));
 
 
 insert into adminDB(Username,Password) values ('Admin','Password');
 
+insert into trainerDB(tId,tFirstName,tLastName,tAge,tGender,tContactNo,tMail,Username,Password,tDateBegin ,tDateEnd ,tstatus) values(1,'Ishan','Agrawal',21,'Male','123456789','ishan@abc.com','ishan123','12345',to_date('01/01/1997','MM/dd/yyyy'),to_date('01/01/2007','MM/dd/yyyy'),0);
+
+insert into smeDB(sId,sFirstName,sLastName,sAge,sGender,sContactNo,sMail,Username,Password,sDateBegin ,sDateEnd ,sstatus) values(1,'Sandeep','Mohanty',21,'Male','124675789','sandeep@abc.com','sandeep321','54123',to_date('08/01/2005','MM/dd/yyyy'),to_date('15/01/2006','MM/dd/yyyy'),0);
+
+insert into sskill(sId ,skillId) values(1,2);
+insert into sskill(sId ,skillId) values(1,3);
+insert into tskill(tId ,skillId) values(1,1);
+insert into tskill(tId ,skillId) values(1,3);
+		   
+insert into cskill(cId ,skillId) values(1,2);
+insert into cskill(cId ,skillId) values(1,3);
+	
+insert into allocationDB(tId,cId,aDateBegin,aDateEnd) values(1,1,to_date('01/08/1999','MM/dd/yyyy'),to_date('18/08/2002','MM/dd/yyyy'));														 in
+														  
 @@getPassHash
 /
 
